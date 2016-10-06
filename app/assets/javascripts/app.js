05d67bd01ce59656b7f386087666b0bc36588d0d
Alexander <yumako87@gmail.com>
2016-10-06 21:52:51 +0200

    Make angular minsafe

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,7 @@
 angular.module('app', ['ionic'])
 
-.controller("ListCtrl", function ($scope, $http) {
+
+.controller('ListCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.data = [];
 
   $http.get("https://ddragon.leagueoflegends.com/cdn/6.5.1/data/en_US/champion.json").then(function (data) {
@@ -10,4 +11,4 @@ angular.module('app', ['ionic'])
       $scope.data.push(champ);
     }
   });
-});
+}]);
